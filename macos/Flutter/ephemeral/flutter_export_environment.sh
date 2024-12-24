70cb5416e90bae9caf22398e2ee8a3e2deba0230
mohamedezz <ezz019960@gmail.com>
2024-12-23 22:12:55 -0800

    added app regex implement api login data flow ( data source , repo , cubit ) added login state validation

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mohamedezz/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/New Volume/train/flutter_advanced_course-features-onboarding"
+export "FLUTTER_APPLICATION_PATH=/Volumes/New Volume/train/doctor_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
